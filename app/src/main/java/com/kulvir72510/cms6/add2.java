cb07bfd244204b939d6341872b17a5ce883f8d35
kulvir72510 <70468419+kulvir72510@users.noreply.github.com>
2020-11-10 13:21:52 +0530

    kps again

--- app/src/main/java/com/kulvir72510/cms6/add2.java
+++ app/src/main/java/com/kulvir72510/cms6/add2.java
@@ -6,15 +6,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class add2 extends AppCompatActivity {
+    EditText model_name,gst,road_price,color,price;
+    Button btn_save;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add2);
+        price = findViewById(R.id.et_phone);
+        color = findViewById(R.id.et_email_id);
+        road_price = findViewById(R.id.et_city);
+        gst = findViewById(R.id.et_address);
+        model_name = findViewById(R.id.tv_email);
+        btn_save = findViewById(R.id.btn_save);
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation2);
         bottomNavigationView.setSelectedItemId(R.id.ic_add);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
--- app/src/main/java/com/kulvir72510/cms6/add3.java
+++ app/src/main/java/com/kulvir72510/cms6/add3.java
@@ -6,15 +6,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class add3 extends AppCompatActivity {
+    EditText model_name,gst,road_price,color,price;
+    Button btn_save;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add3);
+        price = findViewById(R.id.et_phone);
+        color = findViewById(R.id.et_email_id);
+        road_price = findViewById(R.id.et_city);
+        gst = findViewById(R.id.et_address);
+        model_name = findViewById(R.id.tv_email);
+        btn_save = findViewById(R.id.btn_save);
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation3);
         bottomNavigationView.setSelectedItemId(R.id.ic_graph);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
